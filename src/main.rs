eb474c281166eed93f3f69203ca3776ca0a6cd97
Tristram Oaten <tris@oat.sh>
2022-05-08 11:35:49 +0100

    LET THERE BE LIGHT

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+extern crate progressive;
+
+use std::process::Command;
+use std::{thread, time::Duration};
+
+use progressive::progress;
+
+fn main() {
+    println!("Rigging...");
+    for n in progress(14_000..14_301) {
+        freq(n * 1000);
+        thread::sleep(Duration::from_millis(500));
+    }
+}
+
+fn freq(f: u32) {
+    Command::new("rigctl")
+        .args(["-m", "360"])
+        .args(["-r", "/dev/ttyUSB0"])
+        .args(["F", &f.to_string()])
+        .spawn()
+        .expect("failed to execute process");
+}
